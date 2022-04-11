98e37d5ff8940b551d23fd4300e37e1eaaf2ef70
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-04-11 17:27:06 -0400

    getting input to an object

--- src/main/java/JerksonParser.java
+++ src/main/java/JerksonParser.java
@@ -68,20 +68,24 @@ public class JerksonParser {
 
     public static Grocery createObjectFromString(String s) {
         Grocery item = new Grocery();
-        String patternString = "[n,N][a,A][m,M][e,E]";
+        //String patternString = "[n,N][a,A][m,M][e,E]";
+        String patternString = "name";
         String name = getObject(patternString,s);
         if(stringLength(name) == 0) {
             item.setName("Error");
         } else item.setName(name);
-        String patternString1 = "[p,P][r,R][i,I][c,C][e,E]";
+        //String patternString1 = "[p,P][r,R][i,I][c,C][e,E]";
+        String patternString1 = "price";
         String price = getObject(patternString1,s);
         if(stringLength(price) == 0) {
             item.setPrice("Error");
         } else item.setPrice(price);
-        String patternString2 = "[t,T][y,Y][p,P][e,E]";
+        //String patternString2 = "[t,T][y,Y][p,P][e,E]";
+        String patternString2 = "type";
         String type = getObject(patternString2,s);
         item.setType(type);
-        String patternString3 = "[e,E][x,X][p,P][i,I][r,R][a,A][T,t][i,I][o,O][n,N]";
+        //String patternString3 = "[e,E][x,X][p,P][i,I][r,R][a,A][T,t][i,I][o,O][n,N]";
+        String patternString3 = "expiration";
         String expiration = getObject(patternString3,s);
         item.setExpiration(expiration);
         return item;
@@ -90,7 +94,7 @@ public class JerksonParser {
         public static String getObject(String patt, String str){
             int x =0;
             String result = "";
-            Pattern pattern = Pattern.compile(patt);
+            Pattern pattern = Pattern.compile(patt,Pattern.CASE_INSENSITIVE);
             Matcher matcher = pattern.matcher(str);
             if (matcher.find()) {
                 x = matcher.end();
@@ -98,7 +102,7 @@ public class JerksonParser {
             for (int i = x + 1; i < stringLength(str); i++) {
                 result += str.charAt(i);
             }
-            if (patt.equals("[e,E][x,X][p,P][i,I][r,R][a,A][T,t][i,I][o,O][n,N]"))
+            if (patt.equals("expiration"))
                 return result;
             result = getStringFromResult(result);
 
